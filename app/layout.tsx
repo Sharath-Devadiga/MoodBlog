25fc4c0a9e04dd8bf4ae371bac03a5acdc2c4a60
Sharath-Devadiga <sharath13feb@gmail.com>
2025-10-24 21:39:26 +0530

    new icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   title: "MoodBlog",
   description: "Express your thoughts through moods.",
   icons: {
-    icon: '/logo.png',
+    icon: '/new.png',
   },
 };
 
